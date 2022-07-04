90c568d4e1b8ca6f64aa751b42f2a354ad953ca6
Ahmet Durak <98670034+AhmetDurak@users.noreply.github.com>
2022-07-04 17:12:29 +0200

    Update LogoutStepDefs.java

--- src/test/java/com/StepDefinitions/LogoutStepDefs.java
+++ src/test/java/com/StepDefinitions/LogoutStepDefs.java
@@ -31,14 +31,14 @@ public class LogoutStepDefs {
     @Then("User should NOT go to the home page again by clicking go back button")
     public void user_should_not_go_to_the_home_page_again_by_clicking_go_back_button() {
         Driver.get().navigate().back();
-        BrowserUtilities.waitFor(2);
+        BrowserUtilities.waitFor(3);
         logoutPage = new LogoutPage();
         Assert.assertTrue(logoutPage.getAlert.getText().toLowerCase().contains("session expired"));
     }
 
     @Then("User should be landing on Log in page")
     public void userShouldBeLandingOnLogInPage() {
-        BrowserUtilities.waitFor(1);
+        BrowserUtilities.waitFor(2);
         Assert.assertTrue(Driver.get().getTitle().contains("Login"));
     }
 }
